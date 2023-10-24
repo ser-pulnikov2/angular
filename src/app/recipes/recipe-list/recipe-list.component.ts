caeb6985bba6520d25b08efd9aa13a7f13facb64
Sergey <sergey.pulnikov@googlemail.com>
2023-10-24 23:33:08 +0200

    init project

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent  implements OnInit{
+
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe','This is simply a test','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTyy-GmHgUVupRJnFoHnbCTPZoXfF3zRNVx1ST82YcXia-VG4ZP_Q0B0vR8iA5Mk5knSI&usqp=CAU'),
+    new Recipe('A test Recipe','This is simply a test','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTyy-GmHgUVupRJnFoHnbCTPZoXfF3zRNVx1ST82YcXia-VG4ZP_Q0B0vR8iA5Mk5knSI&usqp=CAU')
+  ];
+
+  constructor() {
+  }
+  ngOnInit(): void {
+  }
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Ingridient} from "../shared/ingridient.model";
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent {
+export class ShoppingListComponent implements OnInit{
+  ingridients: Ingridient[] = [
+    new Ingridient('Apples',5),
+    new Ingridient('Tomatoes',10)
+  ];
+
+  constructor() {
+  }
+  ngOnInit(): void {
+  }
 
 }
